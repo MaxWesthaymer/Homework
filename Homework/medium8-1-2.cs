5bc4995347b2819f641af727c8485c13723c9f69
thehdcorp@gmail.com <thehdcorp@gmail.com>
2021-02-05 17:23:09 +0900

    Ex1 complete

--- Homework/medium8-1-2.cs
+++ Homework/medium8-1-2.cs
@@ -9,92 +9,54 @@ namespace Task
         {
             X = x;
             Y = y;
-            this.isAlive = isAlive;
+            IsAlive = isAlive;
         }
 
         public int X { get; set; }
         public int Y { get; set; }
-        public  bool isAlive { get; set; }
+        public bool IsAlive { get; set; }
     }
     class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            int obj1x = 5;
-            int obj1y = 5;
-            bool isalive1 = true;
-            int obj2x = 10;
-            int obj2y = 10;
-            bool isalive2 = true;
-            int obj3x = 15;
-            int obj3y = 15;
-            bool isalive3 = true;
+            Object[] objects = { new Object(5, 5, true),
+                new Object(10, 10, true), 
+                new Object(15, 15, true)
+            };
 
             Random random = new Random();
 
             while (true)
             {
-                if (obj1x == obj2x && obj1y == obj2y)
+                for (int current = 0; current < objects.Length; current++)
                 {
-                    isalive1 = false;
-                    isalive2 = false;
-                }
+                    for (int next = 0; next < objects.Length; next++)
+                    {
+                        if (next <= current)
+                        {
+                            continue;
+                        }
+                        if (objects[current].X == objects[next].X && objects[current].Y == objects[next].Y)
+                        {
+                            objects[current].IsAlive = false;
+                            objects[next].IsAlive = false;
+                        }
+                    }
+                    objects[current].X += random.Next(-1, 1);
+                    objects[current].Y += random.Next(-1, 1);
 
-                if (obj1x == obj3x && obj1y == obj3y)
-                {
-                    isalive1 = false;
-                    isalive3 = false;
-                }
+                    if (objects[current].X < 0)
+                        objects[current].X = 0;
 
-                if (obj2x == obj3x && obj2y == obj3y)
-                {
-                    isalive2 = false;
-                    isalive3 = false;
-                }
-
-                obj1x += random.Next(-1, 1);
-                obj1y += random.Next(-1, 1);
-
-                obj2x += random.Next(-1, 1);
-                obj2y += random.Next(-1, 1);
-
-                obj3x += random.Next(-1, 1);
-                obj3y += random.Next(-1, 1);
-
-                if (obj1x < 0)
-                    obj1x = 0;
-
-                if (obj1y < 0)
-                    obj1y = 0;
-
-                if (obj2x < 0)
-                    obj2x = 0;
-
-                if (obj2y < 0)
-                    obj2y = 0;
-
-                if (obj3x < 0)
-                    obj3x = 0;
-
-                if (obj3y < 0)
-                    obj3y = 0;
-
-                if (isalive1)
-                {
-                    Console.SetCursorPosition(obj1x, obj1y);
-                    Console.Write("1");
-                }
-
-                if (isalive2)
-                {
-                    Console.SetCursorPosition(obj2x, obj2y);
-                    Console.Write("2");
-                }
-
-                if (isalive3)
-                {
-                    Console.SetCursorPosition(obj3x, obj3y);
-                    Console.Write("3");
+                    if (objects[current].Y < 0)
+                        objects[current].Y = 0;
+                    
+                    if (objects[current].IsAlive)
+                    {
+                        Console.SetCursorPosition(objects[current].X, objects[current].Y);
+                        Console.Write($"{current + 1}");
+                    }
                 }
             }
         }
